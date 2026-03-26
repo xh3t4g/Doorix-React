28f3f79e4f3ca947d3ee3f8727569342163f081e
xh3t4g <xh3t4g@gmail.com>
2026-03-26 19:53:32 +0300

    небольшие визуальные улучшения и оптимизация адаптации #2

--- src/components/CardIMG.js
+++
@@ -1,5 +0,0 @@
-export const doors = Object.entries(
-    import.meta.glob('../assets/images/*.jpeg', {eager: true})
-).map(([_path, module]) => ({
-    img: module.default
-}))
--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import reactDOM from 'react-dom/client';
-import App from "./app";
-
-reactDOM.createRoot(document.querySelector('#root')).render(
-    <App />
-)
